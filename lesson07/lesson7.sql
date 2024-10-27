5847a886314340b6002daa1532969e13c07d00b1
iyatsouba <yatsouba@thedigital.gov.ua>
2024-10-27 22:17:22 +0200

    feat(dbs): implement lesson 6&7

---
+++ lesson07/lesson7.sql
@@ -0,0 +1,15 @@
+CREATE TABLE students (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(100)
+);
+
+CREATE OR REPLACE PROCEDURE add_student(student_name VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+   INSERT INTO students ("name") VALUES (student_name);
+END;
+$$;
+
+CALL add_student('John Doe');
+CALL add_student('John Deer');
