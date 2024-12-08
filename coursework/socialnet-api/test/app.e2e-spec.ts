7a3337e1197f396d9b605d8606018e63632dd316
iyatsouba <yatsouba@thedigital.gov.ua>
2024-12-09 01:07:36 +0200

    feat(coursework): added tests

--- coursework/socialnet-api/test/app.e2e-spec.ts
+++ coursework/socialnet-api/test/app.e2e-spec.ts
@@ -1,25 +1,65 @@
-import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import * as request from 'supertest'
 
-import { AppModule } from 'src/app.module';
+import { AppModule } from 'src/app.module'
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('SocialNet Api', () => {
+  let app: INestApplication
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let accessToken: string
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).compile()
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleRef.createNestApplication()
+    await app.init()
+  })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  describe('list posts flow', () => {
+    it(`/POST token`, () => {
+      return request(app.getHttpServer())
+        .post('/api/v1/auth/token')
+        .set('x-client-id', 'test-client')
+        .send({
+          login: 'newMe1',
+          password: 'qwerty12345',
+        })
+        .then((result) => {
+          accessToken = result.body.accessToken
+          expect(result.statusCode).toBe(200)
+          expect(result.body).toMatchObject({
+            accessToken: expect.any(String),
+            refreshToken: expect.any(String),
+          })
+        })
+    })
+
+    it(`/GET posts`, () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/post/list?limit=100&offset=0')
+        .set('x-client-id', 'test-client')
+        .set('authorization', `Bearer ${accessToken}`)
+        .then((result) => {
+          accessToken = result.body.accessToken
+          expect(result.statusCode).toBe(200)
+          expect(result.body).toMatchObject({
+            posts: expect.arrayContaining([
+              expect.objectContaining({
+                author: expect.any(String),
+                content: expect.any(String),
+                date: expect.any(String),
+                id: expect.any(String),
+              }),
+            ]),
+          })
+        })
+    })
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+})
